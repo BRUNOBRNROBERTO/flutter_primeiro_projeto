52fa91fc87f2b7e0f65e5d450195c81d089cd898
Bruno Roberto <brunominasgeraisbrn@gmail.com>
2026-03-04 14:15:43 -0300

    Initial commit

---
+++ lib/pages/home/home/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+enum PopupMenuPages { container }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [
+          PopupMenuButton<PopupMenuPages>(
+            onSelected: (PopupMenuPages valueSelected) {
+              switch (valueSelected) {
+                case PopupMenuPages.container:
+                  Navigator.of(context).pushNamed('/container');
+                  break;
+              }
+            },
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuItem<PopupMenuPages>>[
+                PopupMenuItem<PopupMenuPages>(
+                  value: PopupMenuPages.container,
+                  child: Text('Container'),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
